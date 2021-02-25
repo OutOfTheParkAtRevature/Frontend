ef9d544bc1cce645fadd6b66ee815290e4706c2b
Alan Munoz <41355233+MrSchroeder475@users.noreply.github.com>
2021-02-24 19:29:37 -0600

    Messages with mock data

--- src/app/_services/message.service.ts
+++ src/app/_services/message.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Inbox } from '../_models/inbox';
 import { Message } from '../_models/Message';
+import { MessageInbox } from '../_models/message-inbox';
 import { RecipientList } from '../_models/RecipientList';
 import { UserInbox } from '../_models/UserInbox';
 //import * as signalR from "@aspnet/signalr";
@@ -42,6 +43,10 @@ export class MessageService {
   {
     //return this.http.get(this.baseUrl + 'messages');
     return this.http.get<any>(this.baseUrl + `userInboxes/${userID}`);
+  }
+
+  getMessagesFromConversation(messageID: string) : Observable<MessageInbox>{
+    return this.http.get<MessageInbox>(this.baseUrl + `messages/${messageID}`)
   }
   
   sendMessage(message: Message): Observable<Message>
